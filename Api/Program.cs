3723ccf7dddf7a1a1cacada09da53dfcb287c962
neosmiles <eselen.aig@gmail.com>
2023-06-18 10:11:41 +0100

    docker compose working

--- Api/Program.cs
+++ Api/Program.cs
@@ -11,6 +11,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("ServiceStringMsSql")));
+//Add health check endpoint
+builder.Services.AddHealthChecks();
+//Database connection data, WARNING: DO NEVER USER SA USER, this is just to showcase 
+//var server = Environment.GetEnvironmentVariable("DBServer");
+//var port = Environment.GetEnvironmentVariable("DBPort");
+//var user = Environment.GetEnvironmentVariable("DBUser");
+//var password = Environment.GetEnvironmentVariable("DBPassword");
+//var Database = Environment.GetEnvironmentVariable("Database");
+
+//Database connection for docker
+//string connectionString = $"Server={server},{port};Initial Catalog={Database};User ID={user};Password={password}; TrustServerCertificate=true";
+//builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -32,6 +44,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
